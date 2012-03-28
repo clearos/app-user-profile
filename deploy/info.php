7127236edf1aa21e7c8326d73ee2292e325f1647
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-28 11:39:05 +0000

    - Prep 1.0.10 snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'user_profile';
-$app['version'] = '1.0.6';
+$app['version'] = '1.0.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
