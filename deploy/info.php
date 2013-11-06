cecfdf52efa233d54a0547022e56d2a53183e61c
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-11-06 20:08:06 +0000

    - Prepped for snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'user_profile';
-$app['version'] = '1.5.5';
+$app['version'] = '1.5.10';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -20,6 +20,8 @@ $app['description'] = lang('user_profile_app_description');
 $app['name'] = lang('user_profile_app_name');
 $app['category'] = lang('base_category_my_account');
 $app['subcategory'] = lang('base_subcategory_accounts');
+
+$app['user_access'] = TRUE;
 
 /////////////////////////////////////////////////////////////////////////////
 // Packaging
--- packaging/app-user-profile.spec
+++ packaging/app-user-profile.spec
@@ -1,7 +1,7 @@
 
 Name: app-user-profile
 Epoch: 1
-Version: 1.5.5
+Version: 1.5.10
 Release: 1%{dist}
 Summary: User Profile
 License: GPLv3
