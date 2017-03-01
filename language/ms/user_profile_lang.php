00422150bab706240bc62fedd5adee71510212f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 00:13:58 +0000

    Add new files and directories and commit changes to local repository

--- language/ms/user_profile_lang.php
+++ language/ms/user_profile_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['user_profile_app_description'] = 'The User Profile app is used to change your password and, depending on your system settings, update other profile settings.';
-$lang['user_profile_app_name'] = 'User Profile';
-$lang['user_profile_contact_information'] = 'Contact Information';
+$lang['user_profile_app_description'] = 'Aplikasi Profil Pengguna digunakan untuk menukar kata laluan anda dan, bergantung kepada tetapan sistem anda, mengemas kini tetapan profil lain.';
+$lang['user_profile_app_name'] = 'Profil pengguna';
+$lang['user_profile_contact_information'] = 'Maklumat perhubungan';
