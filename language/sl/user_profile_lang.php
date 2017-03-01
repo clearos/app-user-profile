2fdb1fcf3b9d02bfa40da685510bdc866f8267c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 00:15:43 +0000

    Add new files and directories and commit changes to local repository

--- language/sl/user_profile_lang.php
+++ language/sl/user_profile_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['user_profile_app_description'] = 'The User Profile app is used to change your password and, depending on your system settings, update other profile settings.';
-$lang['user_profile_app_name'] = 'User Profile';
-$lang['user_profile_contact_information'] = 'Contact Information';
+$lang['user_profile_app_description'] = 'Aplikacija Uporabnik se uporablja za spremembo gesla in, odvisno od nastavitev sistema, posodobitev druge nastavitve profila.';
+$lang['user_profile_app_name'] = 'uporabnik';
+$lang['user_profile_contact_information'] = 'Kontaktni podatki';
