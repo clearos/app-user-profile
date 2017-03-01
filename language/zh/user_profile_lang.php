d3ddaff91e50d9547122edb8f6fa0b409711996d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 00:08:49 +0000

    Add new files and directories and commit changes to local repository

--- language/zh/user_profile_lang.php
+++ language/zh/user_profile_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['user_profile_app_description'] = 'The User Profile app is used to change your password and, depending on your system settings, update other profile settings.';
-$lang['user_profile_app_name'] = 'User Profile';
-$lang['user_profile_contact_information'] = 'Contact Information';
+$lang['user_profile_app_description'] = '用户配置文件的应用程序是用来改变您的密码，并根据您的系统设置，更新其他配置文件设置。';
+$lang['user_profile_app_name'] = '用户资料';
+$lang['user_profile_contact_information'] = '联系信息';
