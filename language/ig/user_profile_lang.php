5da13455b05b6ed1ecad2b6cd6903516fb2486ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 00:11:23 +0000

    Add new files and directories and commit changes to local repository

--- language/ig/user_profile_lang.php
+++ language/ig/user_profile_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['user_profile_app_description'] = 'The User Profile app is used to change your password and, depending on your system settings, update other profile settings.';
+$lang['user_profile_app_description'] = 'The User Profile ngwa na-eji ịgbanwe paswọọdụ gị ma, dabere na gị usoro ntọala, imelite ọzọ profaịlụ ntọala.';
 $lang['user_profile_app_name'] = 'User Profile';
-$lang['user_profile_contact_information'] = 'Contact Information';
+$lang['user_profile_contact_information'] = 'Ozi mkpọtụrụ';
