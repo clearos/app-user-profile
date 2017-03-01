11af3882c91f2ca7b2b4e4ef4d26334742532844
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 00:18:15 +0000

    Add new files and directories and commit changes to local repository

--- language/zu/user_profile_lang.php
+++ language/zu/user_profile_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['user_profile_app_description'] = 'The User Profile app is used to change your password and, depending on your system settings, update other profile settings.';
-$lang['user_profile_app_name'] = 'User Profile';
-$lang['user_profile_contact_information'] = 'Contact Information';
+$lang['user_profile_app_description'] = 'Uhlelo lokusebenza User Profile isetshenziselwa ukushintsha iphasiwedi yakho bese, kuye ngokuthi izilungiselelo ohlelweni lwakho, buyekeza nezinye izilungiselelo profile.';
+$lang['user_profile_app_name'] = 'Profile Umsebenzisi';
+$lang['user_profile_contact_information'] = 'Ulwazi lokuxhumana';
